9fffba4b1c5f47e2fc57f84886277d8bc7846e8c
henrique26 <fabricioohh@gmail.com>
2024-07-08 01:57:42 -0300

    refactor: ajusted data card offers

--- src/screens/app/HomeScreen/components/Offers.tsx
+++ src/screens/app/HomeScreen/components/Offers.tsx
@@ -1,17 +1,11 @@
 import { Badge, Icon } from "@components";
 import { theme } from "@theme";
 import { View, Text, StyleSheet, FlatList, Dimensions, TouchableOpacity } from "react-native";
+import { useHome } from "../useHome";
 const { width: screenWidth } = Dimensions.get('window');
 
-
 export function Offers() {
-  const data = [
-    {titleOffer: 'Get 25%', categorie: 'Offer AC Service', color: '#6A9B7E50'},
-    {titleOffer: 'Get 15%', categorie: 'Offer Beauty', color: '#83C1DE50'},
-    {titleOffer: 'Get 24%', categorie: 'Offer AC Service', color: '#CABDFF50'},
-    {titleOffer: 'Get 24%', categorie: 'Offer Applice', color: '#FFD88D50'},
-    {titleOffer: 'Get 24%', categorie: 'Offer AC Service', color: '#6A9B7E50'},
-  ]
+  const {data} = useHome();
 
   return (
     <View style={styles.container}>
--- src/screens/app/HomeScreen/useHome.tsx
+++ src/screens/app/HomeScreen/useHome.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 
 export function useHome() {
-  const [data, setData] = useState(null);
-  // regra de negocio
+  const data = [
+    {titleOffer: 'Get 25%', categorie: 'Offer AC Service', color: '#6A9B7E50'},
+    {titleOffer: 'Get 15%', categorie: 'Offer Beauty', color: '#83C1DE50'},
+    {titleOffer: 'Get 24%', categorie: 'Offer AC Service', color: '#CABDFF50'},
+    {titleOffer: 'Get 24%', categorie: 'Offer Applice', color: '#FFD88D50'},
+    {titleOffer: 'Get 24%', categorie: 'Offer AC Service', color: '#6A9B7E50'},
+  ]
 
   return {
     data,
